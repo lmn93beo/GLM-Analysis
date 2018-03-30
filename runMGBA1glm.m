f105daa80b418a6326e72ab7a38b7523d9d2f12e
Nhat Le <lmn93beo@yahoo.com>
2018-03-30 10:57:51 -0400

    Modified runMGBA1glm.m to have all coupling filters

--- runMGBA1glm.m
+++ runMGBA1glm.m
@@ -2,23 +2,12 @@
 clear all;
 
 full_load = 0;
-source = 'Z:\CheetahData\Pavarrotti mouse\Pavarrotti Mouse\STRFSessionsCombined';
-file = [source '\2017-01-23_15-36-21_STRFMeanSpike.mat'];
-file_short = [source '\2017-01-23_15-36-21_STRFMeanSpike_short.mat'];
+source = 'Z:\CheetahData\Pavarrotti mouse\Pavarrotti Mouse\Session 1\';
+file = [source '2017-01-23_15-36-21_STRFMeanSpike.mat'];
 
 
 if full_load
     load(file);
-
-    % Save only the variables we need
-%     save(file_short, 'num_seq', 'Lstim', 'unit1', 'unit2', 'unit3', 'unit4', ...
-%         'unit5', 'unit6', 'unit7', 'unit8', ...
-%         'unit9', 'unit10', 'unit11', 'unit12', ...
-%         'unit13', 'unit14', 'unit15', 'unit16', ...
-%         'unit17', 'unit18', 'unit19', 'unit20', ...
-%         'unit21', 'unit22', 'unit23', 'unit24', ...
-%         'unit25', 'unit26', 'unit27', 'unit28', ...
-%         'unit29');
 else
     load(file, 'num_seq', 'Lstim', 'numUnits',...
         'unit1', 'unit2', 'unit3', 'unit4', ...
@@ -56,12 +45,12 @@ end
 % Define start and end of 'trials'
 end_id = 594; % decrease in time from t = 594 to 595...
 ntrials = end_id - 1;
-start_times = Lstim.start_time(1:ntrials) + 0.1;
-end_times = Lstim.start_time(1:ntrials) + 1;
+start_times = Lstim.start_time(1:ntrials) + 2;
+end_times = Lstim.start_time(1:ntrials) + 3;
 deadline = Lstim.start_time(2:end_id);
 
 % Add random jitter
-jitter = rand(1, ntrials) * 0.1;
+jitter = rand(1, ntrials) * 0;
 start_times = start_times - jitter;
 end_times = end_times - jitter;
 
@@ -94,6 +83,12 @@ for trial = 1 : end_id - 1
     
     % Store duration in ms
     glmtrial(trial).duration = 1000 * (end_times(trial) - start_times(trial));
+    glmtrial(trial).cueon = 1000;
+    glmtrial(trial).cueoff = 2000;
+    
+ 
+    % Store laser ON/OFF type
+    glmtrial(trial).laser = mod(trial+1, 2);
     
     % Store spike timings
     for unit_id = 1 : num_responsive
@@ -118,13 +113,16 @@ end
 %% Build a GLM object
 % Initialize the experiment with appropriate bin size
 unitOfTime = 'ms';
-binSize = 3;
+binSize = 1;
 
 expt = buildGLM.initExperiment(unitOfTime, binSize);
 
 % Add stimulus timing information
-% expt = buildGLM.registerTiming(expt, 'cueon', 'Cue Onset');
-% expt = buildGLM.registerTiming(expt, 'cueoff','Cue Offset');
+expt = buildGLM.registerTiming(expt, 'cueon', 'Cue Onset');
+expt = buildGLM.registerTiming(expt, 'cueoff','Cue Offset');
+
+% Laser ON/OFF condition
+expt = buildGLM.registerValue(expt, 'laser', 'Laser ON/OFF');
 
 % Spike trains
 % MGB units
@@ -145,6 +143,7 @@ expt.trial = glmtrial;
 
 %% Design specification
 dspec = buildGLM.initDesignSpec(expt);
+binfun = @(t)(t==0)+ceil(t/binSize);
 
 % Add coupling variable from MGB units
 for i = 1:numel(MGB_units)
@@ -155,49 +154,83 @@ for i = 1:numel(MGB_units)
 end
 
 % Design matrix
-trialIndices = 1:end_id - 1;
+trialIndices = 1:2:ntrials;
 dm = buildGLM.compileSparseDesignMatrix(dspec, trialIndices);
 
-%% Get dependent variable
-y = buildGLM.getBinnedSpikeTrain(expt, 'A1Unit20', dm.trialIndices);
+% Some processing on the design matrix
+dm = buildGLM.removeConstantCols(dm);
+dm = buildGLM.addBiasColumn(dm); % DO NOT ADD THE BIAS TERM IF USING GLMFIT
 
-%% Least squares for initialization
-tic
-wInit = dm.X' * dm.X \ dm.X' * y;
-toc
 
-%% Use matRegress for Poisson regression
-% it requires `fminunc` from MATLAB's optimization toolbox
-fnlin = @nlfuns.exp; % inverse link function (a.k.a. nonlinearity)
-lfunc = @(w)(glms.neglog.poisson(w, dm.X, y, fnlin)); % cost/loss function
 
-opts = optimset('Algorithm', 'trust-region-reflective', ...
-    'GradObj', 'on', 'Hessian','on');
+%% Perform the regrssion for each
+ws_all = cell(1, numel(A1_units));
+wvar_all = cell(1, numel(A1_units));
 
-tic
-[wml, nlogli, exitflag, ostruct, grad, hessian] = fminunc(lfunc, wInit, opts);
-wvar = diag(inv(hessian));
-toc
 
-%% Visualize the weights
-ws = buildGLM.combineWeights(dm, wml);
+for A1Num = 1:numel(A1_units)
+    A1Name = sprintf('A1Unit%d', A1_units(A1Num));
+    fprintf('Working on A1 Unit %s...\n', A1Name)
+    
+    %% Get dependent variable
+    y = buildGLM.getBinnedSpikeTrain(expt, A1Name, dm.trialIndices);
 
-figure;
+    %% Least squares for initialization
+    tic
+    wInit = dm.X' * dm.X \ dm.X' * y;
+    toc
 
-% Plot the result
-for i = 1:numel(MGB_units)
-    unit = ['MGBUnit' num2str(MGB_units(i))];
-    weight = ws.(unit).data;
-    time = ws.(unit).tr;
-    plot(time,(ws.(unit).data));
-    hold on;
+    %% Use matRegress for Poisson regression
+    % it requires `fminunc` from MATLAB's optimization toolbox
+    fnlin = @nlfuns.exp; % inverse link function (a.k.a. nonlinearity)
+    lfunc = @(w)(glms.neglog.poisson(w, dm.X, y, fnlin)); % cost/loss function
+
+    opts = optimset('Algorithm', 'trust-region-reflective', ...
+        'GradObj', 'on', 'Hessian','on');
+
+    tic
+    [wml, nlogli, exitflag, ostruct, grad, hessian] = fminunc(lfunc, wInit, opts);
+    wvar = diag(inv(hessian));
+    toc
+
+    %% Visualize the weights
+    ws = buildGLM.combineWeights(dm, wml);
+    wvar = buildGLM.combineWeights(dm, wvar);
+
+    %%
+    %figure;
+
+    coupled_units = [1, 2, 3, 4, 7, 10, 14, 17, 18];
+    ws_all{A1Num} = ws;
+    wvar_all{A1Num} = wvar;
+    
+
+    % Plot the result
+%     for i = 1:numel(dspec.covar)
+%         unit = dspec.covar(i).label;
+%         unitid = str2double(unit(8:end));
+%         weight = ws.(unit).data;
+%         time = ws.(unit).tr;
+%         ws_all(i, A1Num) = ws.(unit).data;
+%         hold on;
+% 
+%         if ismember(unitid, coupled_units)
+%             plot(time,exp(ws.(unit).data), 'r');
+%         else
+%             plot(time, exp(ws.(unit).data), 'k');
+%         end
+        %errorbar(ws.(unit).tr, ws.(unit).data, sqrt(wvar.(unit).data));
+%     end
+
+%     field_names = {dspec.covar.label};
+%     legend(field_names);
 end
 
-field_names = fieldnames(glmtrial);
-legend(field_names(2:end));
-% legend('MGBUnit1', 'MGBUnit2', 'MGBUnit3', 'MGBUnit4', ...
-%     'MGBUnit5', 'MGBUnit6', 'MGBUnit7', 'MGBUnit8',...
-%     'MGBUnit9', 'MGBUnit10', 'MGBUnit11', 'MGBUnit12',...
-%     'MGBUnit13', 'MGBUnit14', 'MGBUnit15', 'MGBUnit16',...
-%     'MGBUnit17', 'MGBUnit18', 'MGBUnit19');
+%% Save!
+filename = 'ws_A1_MGB_all_baseline.mat';
+if ~exist(filename, 'file')
+    save(filename, 'ws_all', 'wvar_all', 'A1_units', 'MGB_units');
+else
+    error('Error: File exists');
+end
 
