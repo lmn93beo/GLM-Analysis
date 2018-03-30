a9d4797f2a91920fffa7c2b660b63c353f276c24
Nhat Le <lmn93beo@yahoo.com>
2018-03-30 11:45:17 -0400

    Added files for plot raster and debug MGBA1glm

--- plotRaster_MGBA1_units.m
+++ plotRaster_MGBA1_units.m
@@ -1,14 +1,12 @@
-function raster_cell = plotRaster_MGBA1_units(glmstruct, unit_num, start_time, end_time)
-% trial_num: number indicating trial
+function raster_cell = plotRaster_MGBA1_units(glmstruct, unit_name, start_time, end_time)
+% unit_name: string indicating name of unit
+% start_time and end_time: indices of start and end timestamps
 
 % Make a cell that contains all units
-field_names = fieldnames(glmstruct);
-raster_cell = {glmstruct.(field_names{unit_num})};
+raster_cell = {glmstruct.(unit_name)};
 raster_cell = raster_cell(1:2:200);
 
 plotRaster(raster_cell);
-%yticks((1:29) + 0.5);
-%yticklabels(field_names(2:end));
 
 % Highlight stimulus period
 patch([start_time end_time end_time start_time], [0 0 numel(raster_cell) numel(raster_cell)], [0.8, 0.8, 0.8], 'FaceAlpha',.3);
--- runMGBA1glm_asMDPFC.m
+++ runMGBA1glm_asMDPFC.m
@@ -8,16 +8,6 @@ file = [source '2017-01-23_15-36-21_STRFMeanSpike.mat'];
 
 if full_load
     load(file);
-
-    % Save only the variables we need
-%     save(file_short, 'num_seq', 'Lstim', 'unit1', 'unit2', 'unit3', 'unit4', ...
-%         'unit5', 'unit6', 'unit7', 'unit8', ...
-%         'unit9', 'unit10', 'unit11', 'unit12', ...
-%         'unit13', 'unit14', 'unit15', 'unit16', ...
-%         'unit17', 'unit18', 'unit19', 'unit20', ...
-%         'unit21', 'unit22', 'unit23', 'unit24', ...
-%         'unit25', 'unit26', 'unit27', 'unit28', ...
-%         'unit29');
 else
     load(file, 'num_seq', 'Lstim', 'numUnits',...
         'unit1', 'unit2', 'unit3', 'unit4', ...
@@ -42,22 +32,12 @@ A1_channels = 10:18;
 % Define which unit is MGB and which is A1
 responsive_only = 1;
 
-% if responsive_only
-%     % unit is MGB or A1 && is auditory-responsive
-%     MGB_units = numUnits(numUnits <= 19);
-%     A1_units = numUnits(numUnits > 19);
-% else
-%     % TODO: Hard coded for now...
-%     MGB_units = 1:19;
-%     A1_units = 20:29;
-% end
-
 MGB_units = [1, 3, 7, 14, 18, 17, 10, 4, 2];
 A1_units = 28;
 
 % Define start and end of 'trials'
 %end_id = 594; % decrease in time from t = 594 to 595...
-ntrials = 100;
+ntrials = 593;
 start_times = Lstim.start_time(1:ntrials);
 end_times = Lstim.start_time(1:ntrials) + 1;
 deadline = Lstim.start_time(2:ntrials+1);
@@ -98,43 +78,8 @@ num_responsive = numel(MGB_units) + numel(A1_units);
 
 % Define trial structures
 for trial = 1 : ntrials
-    
     % Store duration in ms
     glmtrial(trial).duration = 1000 * (end_times(trial) - start_times(trial));
-    glmtrial(trial).cueon = 1000;
-    glmtrial(trial).cueoff = 2000;
-    
- 
-    glmtrial(trial).cueon = 1000;
-    glmtrial(trial).cueoff = 2000;
-    glmtrial(trial).context = 1;
-    glmtrial(trial).vision = 1;
-    glmtrial(trial).audition = 1;
-    glmtrial(trial).lowpasscue = 1;
-    glmtrial(trial).highpasscue = 1;
-    
-    switch floor((trial - 1) / 25)
-        case 0
-            glmtrial(trial).R1C1 = 0;
-            glmtrial(trial).R1C2 = [];
-            glmtrial(trial).R2C1 = [];
-            glmtrial(trial).R2C2 = [];
-        case 1
-            glmtrial(trial).R1C1 = [];
-            glmtrial(trial).R1C2 = 0;
-            glmtrial(trial).R2C1 = [];
-            glmtrial(trial).R2C2 = [];
-        case 2
-            glmtrial(trial).R1C1 = [];
-            glmtrial(trial).R1C2 = [];
-            glmtrial(trial).R2C1 = 0;
-            glmtrial(trial).R2C2 = [];
-        case 3
-            glmtrial(trial).R1C1 = [];
-            glmtrial(trial).R1C2 = [];
-            glmtrial(trial).R2C1 = [];
-            glmtrial(trial).R2C2 = 0;
-    end
     
     % Store spike timings
     for unit_id = 1 : num_responsive
@@ -164,7 +109,7 @@ numPFC = 1;
 
 unitOfTime = 'ms'; 
 binSize = 1;
-nTrials = 100;
+nTrials = 593;
 
 for p = 1
     
@@ -175,43 +120,9 @@ for p = 1
     binSize = 1;
     
     binfun = @(t)(t==0)+ceil(t/binSize);
-%     correct = arrayfun(@(x) glmtrial(x).reward,1:127);
-%     glmtrial = glmtrial(find(correct==1));
     
     %% Specify the fields to load
-    
     expt = buildGLM.initExperiment(unitOfTime, binSize);
-    
-    % values = context
-    expt = buildGLM.registerValue(expt, 'context', 'Context');
-    
-    % timings = cue on, cue off
-    expt = buildGLM.registerTiming(expt, 'cueon', 'Cue Onset');
-    expt = buildGLM.registerTiming(expt, 'cueoff','Cue Offset');
-    
-    % timings = vision, rule
-    % expt = buildGLM.registerTiming(expt, 'vision', 'Attend to Vision Rule');
-    % expt = buildGLM.registerTiming(expt, 'audition', 'Attend to Audition Rule');
-    
-    % rule is value
-    expt = buildGLM.registerTiming(expt, 'vision', 'Attend to Vision Rule');
-    expt = buildGLM.registerTiming(expt, 'audition', 'Attend to Audition Rule');
-    
-    % timings = lpf, hpf
-    expt = buildGLM.registerTiming(expt, 'lowpasscue', 'Low Pass Filter Cue');
-    expt = buildGLM.registerTiming(expt, 'highpasscue', 'High Pass Filter Cue');
-    
-    % timing = decision
-    expt = buildGLM.registerTiming(expt, 'choice', 'Choice Time');
-    
-    % type = rule / context pair
-    expt = buildGLM.registerValue(expt, 'type', 'Trial type');
-    
-    % type v2
-    expt = buildGLM.registerTiming(expt, 'R1C1', 'Rule 1, Context 1');
-    expt = buildGLM.registerTiming(expt, 'R2C1', 'Rule 2, Context 1');
-    expt = buildGLM.registerTiming(expt, 'R1C2', 'Rule 1, Context 2');
-    expt = buildGLM.registerTiming(expt, 'R2C2', 'Rule 2, Context 2');
     
     % spike trains
     for i = 1:numPFC
@@ -221,23 +132,10 @@ for p = 1
         expt = buildGLM.registerSpikeTrain(expt, ['MDUnit' num2str(i)], 'MD Spike Train');
     end
     
-    %% build design spec object and put data in expt
-    
+    %% build design spec object and put data in expt  
     expt.trial = glmtrial;
     dspec = buildGLM.initDesignSpec(expt);
-    
-    %% trial type
-    bs = basisFactory.makeSmoothTemporalBasis('raised cosine', 1000, 15, binfun);
-    % bs = basisFactory.makeSmoothTemporalBasis('boxcar', 600, 16, binfun);
-    offset = 0;
-    dspec = buildGLM.addCovariateTiming(dspec, 'R1C1', [], [], bs, offset);
-    dspec = buildGLM.addCovariateTiming(dspec, 'R2C1', [], [], bs, offset);
-    dspec = buildGLM.addCovariateTiming(dspec, 'R1C2', [], [], bs, offset);
-    dspec = buildGLM.addCovariateTiming(dspec, 'R2C2', [], [], bs, offset);
-    
-    %% spike history
-    % bs = basisFactory.makeSmoothTemporalBasis('boxcar', 24, 12, expt.binfun);
-    % dspec = buildGLM.addCovariateSpiketrain(dspec, 'hist', 'sptrain', 'History filter', bs);
+   
     
     %% add MD coupling
     
@@ -248,13 +146,13 @@ for p = 1
     %% build design matrix
     
     % trialIndices = 1:nTrials;
-    trialIndices = 1:100
+    trialIndices = 1:nTrials;
     dm = buildGLM.compileSparseDesignMatrix(dspec, trialIndices);
     
-    endTrialIndices = cumsum(binfun([expt.trial(trialIndices).duration]));
-    X = dm.X(1:endTrialIndices(5),:);
-    mv = max(abs(X), [], 1); mv(isnan(mv)) = 1;
-    X = bsxfun(@times, X, 1 ./ mv);
+%     endTrialIndices = cumsum(binfun([expt.trial(trialIndices).duration]));
+%     X = dm.X(1:endTrialIndices(5),:);
+%     mv = max(abs(X), [], 1); mv(isnan(mv)) = 1;
+%     X = bsxfun(@times, X, 1 ./ mv);
     
     %%
     
@@ -264,7 +162,7 @@ for p = 1
     dm = buildGLM.removeConstantCols(dm);
     % dm = buildGLM.zscoreDesignMatrix(dm, [colIndices{:}]);
     
-    dm = buildGLM.addBiasColumn(dm); % DO NOT ADD THE BIAS TERM IF USING GLMFIT
+    %dm = buildGLM.addBiasColumn(dm); % DO NOT ADD THE BIAS TERM IF USING GLMFIT
     
     %% Least squares for initialization
     tic
@@ -292,7 +190,7 @@ for p = 1
     wvar = buildGLM.combineWeights(dm, wvar);
     
     ct = 0;
-    for kCov = 5 : 5+numMD-1
+    for kCov = 1 : numMD
         ct = ct+1;
         
         label = dspec.covar(kCov).label;
