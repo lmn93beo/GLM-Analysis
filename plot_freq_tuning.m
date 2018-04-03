6a71b961112de2d8a580296ea482fa26da5a0d26
Nhat Le <lmn93beo@yahoo.com>
2018-04-03 10:50:54 -0400

    Implemented flipping for consistent 3 curves

--- plot_freq_tuning.m
+++ plot_freq_tuning.m
@@ -14,13 +14,13 @@ else
 end
 
 %% Get the right STRF (TODO: talk to Ian)
-strfs = {STRFSmoCell1NL1, STRFSmoCell2NL1, STRFSmoCell3NL1, STRFSmoCell4NL1,...
-    STRFSmoCell7NL1, STRFSmoCell8NL1,...
-    STRFSmoCell9NL1, STRFSmoCell10NL1, STRFSmoCell12NL1,...
-    STRFSmoCell13NL1, STRFSmoCell14NL1, ...
-    STRFSmoCell17NL1, STRFSmoCell18NL1, STRFSmoCell20NL1,...
-    STRFSmoCell22NL1, STRFSmoCell24NL1,...
-    STRFSmoCell27NL1, STRFSmoCell28NL1};
+% strfs = {STRFSmoCell1NL1, STRFSmoCell2NL1, STRFSmoCell3NL1, STRFSmoCell4NL1,...
+%     STRFSmoCell7NL1, STRFSmoCell8NL1,...
+%     STRFSmoCell9NL1, STRFSmoCell10NL1, STRFSmoCell12NL1,...
+%     STRFSmoCell13NL1, STRFSmoCell14NL1, ...
+%     STRFSmoCell17NL1, STRFSmoCell18NL1, STRFSmoCell20NL1,...
+%     STRFSmoCell22NL1, STRFSmoCell24NL1,...
+%     STRFSmoCell27NL1, STRFSmoCell28NL1};
 
 figure;
 
@@ -29,29 +29,16 @@ strf_type = 'STRFSmo';
 for i = 1 : numel(numUnits)
     cellNum = numUnits(i);
     
-    cmd = sprintf('strf1 = %sCell%dNL1;', strf_type, cellNum);
+    cmd = sprintf('strf1 = %sCell%dNL1Post;', strf_type, cellNum);
     eval(cmd);
     
-    cmd = sprintf('strf2 = %sCell%dNL2;', strf_type, cellNum);
+    cmd = sprintf('strf2 = %sCell%dNL2Post;', strf_type, cellNum);
     eval(cmd);
     
-    cmd = sprintf('strf3 = %sCell%dNL3;', strf_type, cellNum);
+    cmd = sprintf('strf3 = %sCell%dNL3Post;', strf_type, cellNum);
     eval(cmd);
-    
-    % Collapse over columns by summing
-    strfCols1 = sum(strf1, 1);
-    [~,posMax1] = max(strfCols1);
-    
-    strfCols2 = sum(strf2, 1);
-    [~,posMax2] = max(strfCols2);
-    
-    strfCols3 = sum(strf3, 1);
-    [~,posMax3] = max(strfCols3);
 
     % Plot the frequency tuning at this maximum
-%     freq_tuning1 = strf1(:, posMax1);
-%     freq_tuning2 = strf2(:, posMax2);
-%     freq_tuning3 = strf3(:, posMax3);
     [U1,~,~] = svd(strf1);
     [U2,~,~] = svd(strf2);
     [U3,~,~] = svd(strf3);
@@ -59,7 +46,15 @@ for i = 1 : numel(numUnits)
     freq_tuning1 = U1(:,1);
     freq_tuning2 = U2(:,1);
     freq_tuning3 = U3(:,1);
-
+    
+    % Flip if corr is negative
+    if corr(freq_tuning1, freq_tuning2) < 0
+        freq_tuning2 = freq_tuning2 * -1;
+    end
+    
+    if corr(freq_tuning1, freq_tuning3) < 0
+        freq_tuning3 = freq_tuning3 * -1;
+    end
 
     
     subplot(9, 2, i);
